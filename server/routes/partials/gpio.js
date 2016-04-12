2f4c65bc510fad32f81c3bec4225464359ac1229
Daniel Abrão <dcerag@br.ibm.com>
2016-04-11 23:27:27 -0300

    I/O fix

--- server/routes/partials/gpio.js
+++ server/routes/partials/gpio.js
@@ -24,31 +24,32 @@
         app.post('/changeLightState', function (req, res) {
             optionSelected = req.body.selectedLight;
             console.log(optionSelected);
-            if (optionSelected) {
-                switch (optionSelected) {
-                    case 1:
-                        handleSwitches(color1, color2);
-                        color0.writeSync(1, function () {
-                            return res.status(200).send("Switch 0 turned on");
-                        });
-                        break;
-                    case 2:
-                        handleSwitches(color0, color2);
-                        color1.writeSync(1, function () {
-                            return res.status(200).send("Switch 1 turned on");
-                        });
-                        break;
-                    case 3:
-                        handleSwitches(color0, color1);
-                        return color2.writeSync(1, function () {
-                            return res.status(200).send("Switch 2 turned on");
-                        });
-                    default:
-                        return res.status(500).send("Unavailable switch");
-                }
-            } else {
-               return res.status(500).send("can't operate without a switch");
+
+            if (optionSelected === 1) {
+                handleSwitches(color1, color2);
+                return color0.writeSync(1, function () {
+                    return res.status(200).send("Switch 0 turned on");
+                });
             }
+
+            if (optionSelected === 2) {
+                handleSwitches(color0, color2);
+                color1.writeSync(1, function () {
+                    return res.status(200).send("Switch 1 turned on");
+                });
+            }
+
+
+            if (optionSelected === 3) {
+                handleSwitches(color0, color1);
+                return color2.writeSync(1, function () {
+                    return res.status(200).send("Switch 2 turned on");
+                });
+            }
+            
+            return res.status(500).send("Unavailable switch");
+
+
 
         });
 
