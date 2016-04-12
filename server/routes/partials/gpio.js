d931124b0df9fa3ba5f840135d1202480033131c
Daniel Abrão <dcerag@br.ibm.com>
2016-04-11 23:58:29 -0300

    debug

--- server/routes/partials/gpio.js
+++ server/routes/partials/gpio.js
@@ -5,20 +5,8 @@
 (function () {
     'use strict';
     var Gpio = require('onoff').Gpio,
-        color0 = new Gpio(18, 'out'),
-        color1 = new Gpio(5, 'out'),
-        color2 = new Gpio(6, 'out'),
-        optionSelected;
 
-    function handleSwitches(switch1, switch2) {
-        console.log('here');
-        try {
-            switch1.writeSync(0);
-            switch2.writeSync(0);
-        } catch (e) {
-            console.log(["Error occurred turning the lights off: ", e].join(""));
-        }
-    }
+        color = [new Gpio(18, 'out'), new Gpio(6, 'out'), new Gpio(5, 'out')];
 
     module.exports = function (app) {
 
@@ -28,44 +16,7 @@
             })
         });
 
-        app.post('/changeLightState', function (req, res) {
-            optionSelected = req.body.selectedLight;
-            console.log(optionSelected);
 
-            if (optionSelected === '1') {
-                handleSwitches(color1, color2);
-                return color0.write(1, function () {
-                    return res.status(200).send("Switch 0 turned on");
-                });
-            }
-
-            if (optionSelected === '2') {
-                handleSwitches(color0, color2);
-                return color1.write(1, function () {
-                    return res.status(200).send("Switch 1 turned on");
-                });
-            }
-
-
-            if (optionSelected === '3') {
-                handleSwitches(color0, color1);
-                return color2.write(1, function () {
-                    return res.status(200).send("Switch 2 turned on");
-                });
-            }
-
-            if (optionSelected === 'special') {
-
-                color0.writeSync(1);
-                color1.writeSync(1);
-                color2.writeSync(1);
-
-                return res.status(200).send('all lights on');
-            }
-
-
-
-        });
 
     };
 
